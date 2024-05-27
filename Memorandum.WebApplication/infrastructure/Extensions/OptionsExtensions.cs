d273ad43cd1416de64938123d59bc1c5fe661bdd
ujm13 <ujm13467926@gmail.com>
2024-05-27 23:43:51 +0800

    fix-修改AddOptionsDependency，將DB連線字串改為從Environment取得

--- Memorandum.WebApplication/infrastructure/Extensions/OptionsExtensions.cs
+++ Memorandum.WebApplication/infrastructure/Extensions/OptionsExtensions.cs
@@ -7,7 +7,17 @@ namespace Memorandum.WebApplication.infrastructure.Extensions
 
         public static IServiceCollection AddOptionsDependency(this IServiceCollection serviceCollection)
         {
-            serviceCollection.AddOptions<DbConnectionOptions>().BindConfiguration("DbConnection");
+            serviceCollection.AddOptions<DbConnectionOptions>()
+            .Configure(o =>
+            {
+                var connection = Environment.GetEnvironmentVariable("Db_Connection");
+                if (connection is null)
+                {
+                    throw new ArgumentException("Env Db_Connection is null");
+                }
+                o.Member = connection;
+            });
+             //.BindConfiguration("DbConnection");
             return serviceCollection;
         }
     }
